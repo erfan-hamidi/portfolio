93a24da10c0a8cf366eb96171be6c1c27b5594cf
erfan hamidi <erfan9757@gimal.com>
2023-12-25 15:33:29 +0330

    media 462px

--- script.js
+++ script.js
@@ -2,7 +2,10 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
-
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x')
+    navbar.classList.toggle('active')
+}
 // scroll sections
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
@@ -31,10 +34,8 @@ window.onscroll = () => {
 
     // remove toggle icon and navbar when click navbar links (scroll)
 
-    menuIcon.onclick = () => {
-        menuIcon.classList.toggle('bx-x')
-        navbar.classList.toggle('active')
-    }
+    menuIcon.classList.remove('bx-x')
+    navbar.classList.remove('active')    
 
     // animation footer on scroll
 
